ad21273a6dbc1ab07a135575f1dff1dcd13f1187
fguilhermepll <fguilhermepll@gmail.com>
2022-03-13 21:55:18 -0300

    feat: bird-count

---
+++ bird-count/lib/bird_count.ex
@@ -0,0 +1,18 @@
+defmodule BirdCount do
+  def today([today | tail]), do: today
+  def today([]), do: nil
+
+  def increment_day_count([today | tail]), do: [today + 1 | tail]
+  def increment_day_count([]), do: [1]
+
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | rest]), do: true
+  def has_day_without_birds?([today | rest]), do: has_day_without_birds?(rest)
+
+  def total([]), do: 0
+  def total([count | rest]), do: count + total(rest)
+
+  def busy_days([]), do: 0
+  def busy_days([today | rest]) when today >= 5, do: 1 + busy_days(rest)
+  def busy_days([_ | rest]), do: busy_days(rest)
+end
