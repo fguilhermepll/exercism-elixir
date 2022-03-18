574be7a63d37083261d0d010e83e762e50b45e4a
fguilhermepll <fguilhermepll@gmail.com>
2022-03-17 23:54:22 -0300

    city-office

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -4,13 +4,13 @@ defmodule HighScore do
   end
   
   @default_score 0
-  def add_player(scores, name, score \\ @default_score), do: Map.put(scores, name, score)
+  def add_player(scores, name, score \\ @default_score), do:
   
-  def remove_player(scores, name), do: Map.delete(scores, name)
+  def remove_player(scores, name), do: 
   
-  def reset_score(scores, name), do: Map.put(scores, name, @default_score)
+  def reset_score(scores, name), do: 
   
-  def update_score(scores, name, score), do: Map.update(scores, name, score, fn current_score -> current_score + score end)
+  def update_score(scores, name, score), do: 
   
-  def get_players(scores), do: Map.keys(scores)
+  def get_players(scores), do:
 end
